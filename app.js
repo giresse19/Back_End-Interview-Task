d0fabaeabef23fde0fa371f63af3ab7c40ee205f
giresse bob <ashubobg@yahoo.co.uk>
2017-06-03 15:43:05 +0000

    final work

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
+    user: 'giresse19',
     database: 'be_task'
 });
 app.use(bodyParser.json());
